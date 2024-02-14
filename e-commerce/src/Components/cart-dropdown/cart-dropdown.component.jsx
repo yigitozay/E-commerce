5be9fca9e8f9d6913111c44c1859306a65108bbc
yigitozay <yigit.ozay344@gmail.com>
2024-02-14 03:23:41 +0100

    checkout
    created checkoutpage along checkout items and cart functionality.

--- e-commerce/src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ e-commerce/src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,8 +3,13 @@ import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart-context';
+import { useNavigate } from 'react-router-dom';
 const CartDropdown = () =>{
     const {cartItems} =useContext(CartContext);
+    const navigate = useNavigate();
+    const goToCheckoutHandler = () =>{
+        navigate('/checkout')
+    }
     return (
         <div className='cart-dropdown-container'>
           <div className='cart-items'>
@@ -16,7 +21,7 @@ const CartDropdown = () =>{
               <span className='empty-message'>Your cart is empty</span>
             )}
           </div>
-          <Button>CHECKOUT</Button>
+          <Button onClick= {goToCheckoutHandler}>CHECKOUT</Button>
         </div>
       );
     };
